81bbf083d5a1b4299e7c8b359ca257ab0d85ae2d
Mikołaj Kąkol <mikolaj.kakol@gmail.com>
2023-12-10 20:16:16 +0100

    day 9

---
+++ src/day09/DaySolved.kt
@@ -0,0 +1,50 @@
+package day09
+
+import Day
+import DayTest
+import org.junit.Test
+import util.retrieveNumbers
+
+object DaySolved : Day {
+
+    override fun part1(input: List<String>) = input
+        .sumOf { it.retrieveNumbers().predictNext() }
+
+    override fun part2(input: List<String>) = input
+        .sumOf {
+            it.retrieveNumbers()
+                .predictNext(
+                    operation = { a, b -> a - b },
+                    select = { first() },
+                )
+        }
+
+    private fun Iterable<Int>.predictNext(
+        operation: (Int, Int) -> Int = { a, b -> a + b },
+        select: Iterable<Int>.() -> Int = { last() },
+    ): Int {
+        val newEval = windowed(2, 1)
+            .map { (a, b) -> b - a }
+
+        return if (newEval.all { it == 0 }) {
+            operation(select(this), select(newEval))
+        } else {
+            operation(select(this), newEval.predictNext(operation, select))
+        }
+    }
+}
+
+class DaySolvedTest : DayTest(DaySolved, true) {
+
+    @Test
+    fun testPart1() = testPart1(114)
+
+    @Test
+    fun realPart1() = realPart1(1930746032)
+
+    @Test
+    fun testPart2() = testPart2(2)
+
+    @Test
+    fun realPart2() = realPart2(1154)
+}
--- src/util/parse.kt
+++ src/util/parse.kt
@@ -1,14 +1,14 @@
 package util
 
 fun String.retrieveNumbers(): List<Int> {
-    return "[0-9]+".toRegex()
+    return "-*[0-9]+".toRegex()
         .findAll(this)
         .map { it.value.toInt() }
         .toList()
 }
 
 fun String.retrieveLongNumbers(): List<Long> {
-    return "[0-9]+".toRegex()
+    return "-*[0-9]+".toRegex()
         .findAll(this)
         .map { it.value.toLong() }
         .toList()
